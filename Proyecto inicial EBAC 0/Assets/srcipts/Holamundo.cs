a4890ee192ec3dad1e217ac1634eadb4ea6eb9a3
BeatlHawk <assasinpreance@gmail.com>
2024-09-02 20:46:00 -0600

    pruebas con Visual Studio
    Se realizaron pruebas en visual studio para la clase de EBAC

--- Proyecto inicial EBAC 0/Assets/srcipts/Holamundo.cs
+++ Proyecto inicial EBAC 0/Assets/srcipts/Holamundo.cs
@@ -4,18 +4,50 @@ using UnityEngine;
 
 public class Holamundo : MonoBehaviour
 {
+    private void Awake()
+    {
+        Debug.Log("Hola desde Awake");
+    }
+    //int x;
     // Start is called before the first frame update
     void Start()
     {
-        print("algo paso");
-        Debug.Log("algo paso");
-        Debug.LogWarning("algo salio medianamente mal");
-        Debug.LogError("algo salio muy mal");
+        Debug.Log("hola desde Start");
+        //x = 0;
+        //print("algo paso");
+
+        //Debug.LogWarning("algo salio medianamente mal");
+        //Debug.LogError("algo salio muy mal");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1; 
+        //Debug.Log(x);
+
+        Debug.Log("HolaDesdeUpdate");
     }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("Hola Desde FixedUpdate");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola Desde Late Update");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("Hola desde OnEnable");
+    }
+
+
+    private void OnDisable()
+    {
+        Debug.Log("Hola desde OnDisable");
+    }
+
 }
